1762acf370f18b797fc9e399299dbf6b374542fa
bpena <bernardo.pena.ramos@gmail.com>
2019-09-21 00:07:29 -0400

    using bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'H/F Valencia';
+  todos: Observable<any[]>;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.todos = this.http.get<any[]>('https://jsonplaceholder.typicode.com/todos');
+  }
 }
