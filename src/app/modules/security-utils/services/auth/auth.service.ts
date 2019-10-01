60462971c90d33b77c1a69d2caeaf3ad63bd57c5
bpena <bernardo.pena.ramos@gmail.com>
2019-10-01 15:52:54 -0400

    updating security to use localstorage

--- src/app/modules/security-utils/services/auth/auth.service.ts
+++ src/app/modules/security-utils/services/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { from, BehaviorSubject } from 'rxjs';
 import { tap, catchError } from 'rxjs/operators';
+import { isNullOrUndefined } from 'util';
 
 @Injectable({
     providedIn: 'root'
@@ -9,14 +10,20 @@ import { tap, catchError } from 'rxjs/operators';
 export class AuthService {
     private signed$: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
-    constructor(private afAuth: AngularFireAuth) { }
+    constructor(private afAuth: AngularFireAuth) {
+        this.signed.next(!isNullOrUndefined(localStorage.getItem('uuid')));
+    }
 
     get signed() { return this.signed$; }
 
     login(email: string, password: string) {
         return from(this.afAuth.auth.signInWithEmailAndPassword(email, password))
             .pipe(
-                tap(user => this.signed.next(true)),
+                tap(user => {
+                    console.log(user);
+                    localStorage.setItem('uuid', user.user.uid);
+                    this.signed.next(true);
+                }),
                 catchError(error => {
                     this.signed.next(false);
                     return error;
@@ -27,7 +34,10 @@ export class AuthService {
     logout() {
         return from(this.afAuth.auth.signOut())
             .pipe(
-                tap(value => this.signed.next(false))
+                tap(value => {
+                    localStorage.removeItem('uuid');
+                    this.signed.next(false);
+                })
             );
     }
 }
